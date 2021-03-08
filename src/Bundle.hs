82c4041661fff0117c437675e06ab0438d07c869
Dr. ERDI Gergo <gergo@erdi.hu>
2021-03-08 19:14:08 +0800

    Swap order of `Bundle` params

--- src/Bundle.hs
+++ src/Bundle.hs
@@ -5,32 +5,32 @@ module Bundle where
 
 import Clash.Prelude hiding (Bundle(..), mealyB)
 
-class Bundle a f res | f a -> res, f res -> a where
+class Bundle f a res | f a -> res, f res -> a where
     bundle :: (Applicative f) => res -> f a
 
-    default bundle :: (Applicative f, res ~ f a) => res -> f a
+    default bundle :: (res ~ f a) => res -> f a
     bundle = id
 
     unbundle :: (Traversable f) => f a -> res
 
-    default unbundle :: (Traversable f, res ~ f a) => f a -> res
+    default unbundle :: (res ~ f a) => f a -> res
     unbundle = id
 
-instance Bundle Bit f (f Bit) where
+instance Bundle f Bit (f Bit) where
 
-instance Bundle (a, b) f (f a, f b) where
+instance Bundle f (a, b) (f a, f b) where
     bundle (x, y) = (,) <$> x <*> y
     unbundle xy = (fst <$> xy, snd <$> xy)
 
-instance (KnownNat n) => Bundle (Vec n a) f (Vec n (f a)) where
+instance (KnownNat n) => Bundle f (Vec n a) (Vec n (f a)) where
     bundle = traverse# id
     unbundle = sequenceA . fmap lazyV
 
-instance Bundle (Maybe a) f (f (Maybe a)) where
+instance Bundle f (Maybe a) (f (Maybe a)) where
 
 mealyB
   :: (HiddenClockResetEnable dom)
-  => (Bundle i (Signal dom) i', Bundle o (Signal dom) o')
+  => (Bundle (Signal dom) i i', Bundle (Signal dom) o o')
   => (NFDataX s)
   => (s -> i -> (s,o))
   -> s
@@ -38,7 +38,7 @@ mealyB
 mealyB = hideClockResetEnable mealyBE
 
 mealyBE
-  :: (Bundle i (Signal dom) i', Bundle o (Signal dom) o')
+  :: (Bundle (Signal dom) i i', Bundle (Signal dom) o o')
   => (NFDataX s)
   => Clock dom
   -> Reset dom
